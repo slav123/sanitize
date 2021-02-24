bdcfbbbfc72185de8bf5ba04e0365e3a982bb69c
slav123 <slav123@gmail.com>
2021-02-24 10:08:17 +0100

    first commit

---
+++ sanitize_test.go
@@ -0,0 +1,25 @@
+package sanitize
+
+import "testing"
+
+func TestStringToAsciiBytes(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult string
+	}{
+		{"Test 1", args{"Sławomir Jasiński"}, "Slawomir Jasinski"},
+			{"Test 2", args{"Gżegżółka"}, "Gzegzolka"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotResult := Accents(tt.args.s); gotResult != tt.wantResult {
+				t.Errorf("Accents() = %v, want %v", gotResult, tt.wantResult)
+			}
+		})
+	}
+}
+
